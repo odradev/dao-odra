0fc678a44de8a974fdf5303af0862376eed8aad6
zie1ony <maciej@odra.dev>
2023-06-13 12:23:47 +0200

    lint

--- dao/tests/test_bid_escrow.rs
+++ dao/tests/test_bid_escrow.rs
@@ -8,13 +8,14 @@ use cucumber::World as _;
 fn main() {
     let runner = DaoWorld::cucumber()
         .with_writer(Libtest::or_basic())
-        .with_runner(cucumber_runner::SyncRunner::default()
-            .with_before_scenario(|scenario| {
-                println!("Before scenario: \"{}\"", scenario.name);
-            })
-            .with_after_scenario(|scenario| {
-                println!("After scenario: \"{}\"", scenario.name);
-            })
+        .with_runner(
+            cucumber_runner::SyncRunner::default()
+                .with_before_scenario(|scenario| {
+                    println!("Before scenario: \"{}\"", scenario.name);
+                })
+                .with_after_scenario(|scenario| {
+                    println!("After scenario: \"{}\"", scenario.name);
+                }),
         )
         .run_and_exit("tests/features/bid_escrow/");
     futures::executor::block_on(runner);
